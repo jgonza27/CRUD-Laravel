1ea566d9c4e72c0743705add70c95fdaed582a91
Tu Nombre <tu_correo@ejemplo.com>
2026-02-03 10:06:19 +0100

    a

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,21 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    
+     @var list<string>
+     
     protected $fillable = [
         'name',
         'email',
@@ -24,21 +20,16 @@ class User extends Authenticatable
         'google_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    
+     @var list<string>
+     
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    @return array<string, 
+     
     protected function casts(): array
     {
         return [
@@ -47,9 +38,7 @@ class User extends Authenticatable
         ];
     }
 
-    /**
-     * Get the articles for the user.
-     */
+    
     public function articles()
     {
         return $this->hasMany(\App\Models\Article::class);
